a499ec0ac72f832852394323991ed6a9f511ad16
Ali Talip ÇALIKOĞLU <acalikoglu@outlook.com>
2022-02-10 12:27:52 +0300

    Create install-cockpit.sh

---
+++ install-cockpit.sh
@@ -0,0 +1,12 @@
+####################### install cockpit #######################
+# sudo apt install -t focal-backports cockpit # for cockpit version 215 on ubuntu 20.04.3
+sudo apt install \
+cockpit \
+cockpit-389-ds \
+cockpit-doc \
+cockpit-machines \
+cockpit-pcp \
+cockpit-tests \
+cockpit-storaged \
+-y
+###############################################################
