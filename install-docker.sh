8294988955f72fde87c5b206275048b6a49f1da1
Ali Talip ÇALIKOĞLU <acalikoglu@outlook.com>
2022-02-10 12:30:11 +0300

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,11 @@
+################## install docker and portainer ###############
+sudo apt install ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+###############################################################
