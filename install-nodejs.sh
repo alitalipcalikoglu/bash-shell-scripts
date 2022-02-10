f264d0d54724bb45db29cf058f65ad593309f89e
Ali Talip ÇALIKOĞLU <acalikoglu@outlook.com>
2022-02-10 17:07:48 +0300

    Update install-nodejs.sh

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,4 +1,5 @@
 ####################### install nodejs & npm ##################
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install nodejs npm -y
+sudo apt install nodejs -y
+sudo apt install npm -y
 ###############################################################
