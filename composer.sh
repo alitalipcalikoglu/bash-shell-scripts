e135c0f39fe9d6649f5b3acd3a39356fc8966f38
Ali Talip ÇALIKOĞLU <acalikoglu@outlook.com>
2022-03-12 15:04:18 +0300

    Update composer.sh

--- composer.sh
+++ composer.sh
@@ -1,5 +1,5 @@
 ################### install composer ############################
-sudo apt install curl -y
+sudo apt install curl php-cli unzip -y
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --version=1.10.17 --install-dir=/usr/local/bin --filename=composer
 php composer-setup.php
